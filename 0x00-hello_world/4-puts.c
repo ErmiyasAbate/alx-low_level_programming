748621f585029279c059059125e546cd17a9ad6b
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-03-16 07:42:06 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ *main - Entry Point of the function a line with puts
+ *
+ *Return: 0 for succesful exit
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
